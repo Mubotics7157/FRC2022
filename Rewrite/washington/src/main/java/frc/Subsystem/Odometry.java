18b7b23f2ea956de5d266c55d13f9e87958e0c0c
Aaron Yoon <awesomeyooner@gmail.com>
2022-04-04 15:34:02 -0700

    made different objects to better differentiate the values

--- Rewrite/washington/src/main/java/frc/Subsystem/Odometry.java
+++ Rewrite/washington/src/main/java/frc/Subsystem/Odometry.java
@@ -2,12 +2,15 @@ package frc.Subsystem;
 
 import java.lang.reflect.Field;
 
+import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.Subsystem.Drive.DriveState;
@@ -21,9 +24,16 @@ public class Odometry extends AbstractSubsystem{
     Field2d m_field;
     private static Odometry instance = new Odometry();
 
+    Matrix<N3,N1> stateDev;
+    Matrix<N1,N1> localDev;
+    Matrix<N3,N1> visionDev;
+
     private Odometry(){
         super(20,20);
-
+        stateDev = VecBuilder.fill(0.015, 0.015, 0.015);
+        localDev = VecBuilder.fill(0.015);
+        visionDev = VecBuilder.fill(0.015, 0.015, 0.015);
+        
         m_field = new Field2d();
         SmartDashboard.putData("field", m_field);
 
@@ -31,9 +41,9 @@ public class Odometry extends AbstractSubsystem{
             Drive.getInstance().getDriveHeading(),
             getOdometry(),
             DriveConstants.DRIVE_KINEMATICS,
-            VecBuilder.fill(x, y, theta), //state
-            VecBuilder.fill(theta), //local
-            VecBuilder.fill(x, y, theta), //vision
+            stateDev, //state
+            localDev, //local
+            visionDev, //vision
             0.02
             );
     }
