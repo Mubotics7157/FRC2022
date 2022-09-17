5b697ff1ce1e62e31d3e73e99c6de68c112dcd69
Vernonicuhhh <urmumalad@gmail.com>
2022-09-17 15:21:15 -0700

    Update Constants.java

--- Rewrite/washington/src/main/java/frc/robot/Constants.java
+++ Rewrite/washington/src/main/java/frc/robot/Constants.java
@@ -112,7 +112,6 @@ public interface Constants {
 
         }
 
-    }
     public static final class ShooterConstants{
         public static final int DEVICE_ID_TOP_WHEEL = 19;
         public static final int DEVICE_ID_BOT_WHEEL = 20;
